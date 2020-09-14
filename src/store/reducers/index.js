298eb4a898e8c673a3c14040ca7bd0d60e5f5e65
dzinoviev <danzino21@gmail.com>
2020-09-14 09:36:11 +0300

    Initial redux template

---
+++ src/store/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {};
+
+const reducer = (state, action) => {
+  if (!state) {
+    return initialState;
+  }
+
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export { reducer };
